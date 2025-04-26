539d13b8daa46394e5e0aee0d6b8c3e938ade695
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-04-26 14:27:53 -0700

    configured routes.

---
+++ server/src/routes/htmlRoutes.js
@@ -0,0 +1,22 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Router } from 'express';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const router = Router();
+
+//Define route to serve index.html
+//!!! I don't think I need req since this is JavaScript and I'm not using req
+router.get('/', (res) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+  console.log(`PATH NAME: ${__dirname}/../../../client/dist/index.html`);
+});
+
+//!!! I don't think I need req since this is JavaScript and I'm not using req
+router.get('*', (res) => {
+  res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+  console.log(`PATH NAME: ${__dirname}/../../../client/dist/index.html`);
+});
+
+
+export default router;
