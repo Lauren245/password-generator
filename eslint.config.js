c73f10be795f514e45deb37a85be080004c7a60c
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-04-26 14:42:14 -0700

    added more ESLint rules and fixed the errors they raised.

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,69 @@
-import js from '@eslint/js';
+// import js from '@eslint/js';
 import globals from 'globals';
 
 export default [
-    { ignores: ['dist', 'build'] },
+  { ignores: ['dist', 'build'] },
 
-    //Client-side config
-    {
-        files: ['client/**/*.js'],
-        languageOptions: {
-            ecmaVersion: 2020,
-            sourceType: 'module',
-            globals: globals.browser,
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            indent: ['error', 2],
-        },
+  //Client-side config
+  {
+    files: ['client/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
     },
+    rules: {
+      //Formatting & Style
+      'no-multi-spaces': 'error',
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'semi': ['error', 'always'],
+      'indent': ['error', 2],
+      'object-curly-spacing': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-spacing': ['error', { before: true, after: true }],
 
-    //Server-side config
-    {
-        files: ['server/**/*.js'],
-        languageOptions: {
-            ecmaVersion: 2020,
-            sourceType: 'module',
-            globals: globals.node,
-        },
+      //Best Practices
+      //'no-console': 'warn',
+      'eqeqeq': ['error', 'always'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-duplicate-imports': 'error',
+      'prefer-const': ['error', {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      }],
     },
-    {
-        rules: {
-            ...js.configs.recommended.rules,
-            indent: ['error', 2],
-        },
-    },
+  },
 
-]
+  //Server-side config
+  {
+    files: ['server/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.node,
+    },
+  },
+  {
+    rules: {
+      //Formatting & Style
+      'no-multi-spaces': 'error',
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'semi': ['error', 'always'],
+      'indent': ['error', 2],
+      'object-curly-spacing': ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+
+      //Best Practices
+      //'no-console': 'warn',
+      'eqeqeq': ['error', 'always'],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-duplicate-imports': 'error',
+      'prefer-const': ['error', {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      }],
+    },
+  },
+
+];
